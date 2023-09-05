793f89360df7b15700b578c63a97fe173950a98d
UncleLion <unclelion@ukr.net>
2023-09-05 23:17:12 +0300

    upgrade and improve form, with styles to her

--- src/Header/LoginPage.tsx
+++ src/Header/LoginPage.tsx
@@ -13,56 +13,36 @@ function LoginPage() {
 
     return (
         <div className="container form-group">
-            <div className='row col-md-6 '>
-                <h2 className='mb-3'>Login Form</h2>
-                <form className='px-3'>
-                    <label htmlFor='email' className='form-label'>Email Address</label>
-                    <div className='form-input'>
+            <form className='mx-auto'>
+
+                    <h2 className='mb-3'>Login Form</h2>
+                
+                    <div className='form-input mb-3'>
                         <span><i className='bi bi-envelope-at'></i></span>
-                        <input type="email" className='form-control form-control-lg bg-light ' required placeholder='Email' />
+                        <input type="email" className='form-control bg-light' required placeholder='Email' />
                     </div>
-                    <div className='invalid-feedback'>
-                        Please Enter you email
-                    </div>
-                    <label htmlFor='password' className='form-label'>Password</label>
+
                     <div className='form-input'>
                         <span><i className='bi bi-lock'></i></span>
                         <input
                         value={password}
                         type={visible ? 'text' : 'password'}
+                        className='form-control bg-light'
                         id='password'
-                        onChange={(e) => setPassword(e.target.value)}
-                        className='form-control'
-                        required
                         placeholder='Password'
-                    />
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        />
+                        <div className='icons' onClick={() => setVisible(!visible)}>
+                        {visible ? <span><i className="bi bi-eye"></i></span> : <span><i className="bi bi-eye-slash-fill"></i></span>}
                     </div>
-                        
-                    <div className='icons' onClick={() => setVisible(!visible)}>
-                        {visible ? <i className="bi bi-eye"></i> : <i className="bi bi-eye-slash-fill"></i> }
-                    </div>
-
-                    <div className='invalid-feedback'>
-                        Please Enter you password
-                    </div>
-                    <div className='row mb-3'>
-                        <div className='col-auto d-flex align-items-center'>
-                            <div className='form-check custom-checkbox'>
-                                <input type="checkbox" className='form-check-input'/>
-                                <label className='form-check-label'>Remember me</label>
-                            </div>
-                        </div>
-                    </div>
-                   
                     <div className="forgot">
                         <a href='#'>Forgot Password</a>
                     </div>
-
-                    <div className='mb-3'>
-                        <button type='submit' className='btn btn-block'>SIGN IN</button>
                     </div>
-                </form>
-            </div>   
+                   
+                    <button type='submit' className='btn btn-block'>SIGN IN</button>
+             </form>  
         </div> 
     )
 }
